f55155944067ecb3a759d452442308c923fa07b2
guymorganb <guymorganb@gmail.com>
2023-07-03 15:12:01 -0500

    added models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,32 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// import our database connection from config.js
 const sequelize = require('../config/connection.js');
-
+// Initialize Category model (table) by extending off Sequelize's Model class
 class Category extends Model {}
-
+// set up fields and rules for Category model (These should match up with you schema.sql)
 Category.init(
   {
-    // define columns
+    // this object defines the attributes of the table sequelize is connecting to inside the sql database
+    id: {
+      // define columns
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
+  // must be a second argument of the init() command not part of the object that defines the attributes of the table
   {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    timestamps: false,  // This disables the createdAt and updatedAt timestamps that Sequelize automatically adds to models by default.
+    freezeTableName: true, // This prevents Sequelize from pluralizing the model name to create the table name.
+    underscored: true,  // This makes the auto-generated fields of createdAt and updatedAt (if you didn't disable timestamps) become created_at and updated_at, which is more aligned with SQL's convention.
+    modelName: 'category',  // modelName: The name of the model. Sequelize uses this to create the name of the table in the database.
   }
 );
 
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -5,11 +5,43 @@ const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
-
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // this object defines the attributes of the table sequelize is connecting to inside the sql database
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+   },
+   product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+   }, 
+   price: {
+    type: DataTypes.DECIMAL(10,2), // be sure to set the precision same as sql in this case (10,2)
+    allowNull: false,
+    validate: {   // dont forget to validate that its a decimal
+      isDecimal: true,
+    },
+   },
+   stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10, // dont forget the default values
+    validate: {    // dont forget to validate that its a number
+      isNumeric: true,
+    },
+   },
+   category_id: {
+    type: DataTypes.INTEGER,
+    // include the references to the 
+    references: {
+      model: 'category', // 'category' refers to the table name
+      key: 'id',  // 'id' refers to the column name in category table    
+    },
+   },
   },
   {
     sequelize,
